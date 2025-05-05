ff3fa542d02b828009d113534b66cb1744863ee3
Nathan Licow <nathanlicow@MacBook-Air-von-Nathan.local>
2025-05-05 11:57:21 +0200

    Multiplikation, Addition, Subtraktion fertig

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -2,26 +2,26 @@
     <h1 class=" text-5xl text-center m-12">Matrix Rechner!</h1>
 
         <div class="flex justify-around w-full ">
+            <!--Matrix A-->
             <div class=" ml-6 inline-block p-10 bg-white shadow-2xl rounded-xl border-1 border-gray-400"> 
                 <div class="flex flex-col items-center">
                     <h2 class=" mb-4 mt-4 text-lg">Matrix A</h2>
                     <div class="ml-1 mb-4">
-                        <button v-on:click="A_spalten_adder" 
+                        <button v-on:click="() => {A_spalten_adder(); dim_checker()}" 
                         class=" rounded bg-gray-900 h-10 w-10 text-white">+
                         </button>
-                        <button v-on:click="A_spalten_remover" 
+                        <button v-on:click="() => {A_spalten_remover(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white ml-1">-
                         </button>
-                        <button v-on:click="A_zeilen_adder" 
+                        <button v-on:click="() => {A_zeilen_adder(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white ml-4">+
                         </button>
-                        <button v-on:click="A_zeilen_remover" 
+                        <button v-on:click="()=> {A_zeilen_remover(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white ml-1">-
                         </button>
                     </div>
                 </div>
-                
-                <!--Matrix A-->
+            
                 <table>
                     <tr v-for="(zeile,i) in A_matrix" :key="i">
                         <td v-for="(value, j) in zeile" :key="j">
@@ -33,26 +33,49 @@
                 </table>
             </div>
 
-            <!--Multiplikation-->
+            <!--Operationen Buttons-->
+            <div class="flex flex-col">
+                <!--Multiplikation-->
+                <button
+                    v-on:click="() => {matrix_multiplikator()}"
+                    class="bg-gray-900 h-10 w-30 text-white text-center transition-colors duration-300 mt-12 mb-6"
+                    :class="{ 'hover:bg-red-700': !dim_multi}"
+                    >A * B
+                </button>
 
-            <button v-on:click="update_C_matrix" class=" bg-green-800 h-10 w-30 text-center">A * B
-            </button>
+                <!--Addition-->
+                <button
+                    v-on:click="() => {matrix_addierer()}"
+                    class="bg-gray-900 h-10 w-30 text-center transition-colors duration-300 mb-6 text-white"
+                    :class="{ 'hover:bg-red-700': !dim_addition}"
+                    >A + B
+                </button>
+
+                <!--Subraktion-->
+                <button
+                    v-on:click="() => {matrix_subtrahierer()}"
+                    class="bg-gray-900 text-white h-10 w-30 text-center transition-colors duration-300"
+                    :class="{ 'hover:bg-red-700': !dim_addition}"
+                    >A - B
+                </button>
+            </div>
+
             
             <!--MATRIX B-->
-            <div class=" ml-6 inline-block p-10 bg-white shadow-2xl rounded-xl border-1 border-gray-400 mr-4"> 
+            <div class="inline-block p-10 bg-white shadow-2xl rounded-xl border-1 border-gray-400 mr-4"> 
                 <div class="flex flex-col items-center">
                     <h2 class=" mb-4 mt-4 text-lg">Matrix B</h2>
                     <div class="flex justify-end mr-4 mb-4">
-                        <button v-on:click="B_spalten_adder" 
+                        <button v-on:click="() => {B_spalten_adder(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white">+
                         </button>
-                        <button v-on:click="B_spalten_remover" 
+                        <button v-on:click="() => {B_spalten_remover(); dim_checker()}" 
                         class=" rounded bg-gray-900 h-10 w-10 text-white ml-1">-
                         </button>
-                        <button v-on:click="B_zeilen_adder" 
+                        <button v-on:click="() => {B_zeilen_adder(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white ml-4">+
                         </button>
-                        <button v-on:click="B_zeilen_remover" 
+                        <button v-on:click="() => {B_zeilen_remover(); dim_checker()}" 
                         class="rounded bg-gray-900 h-10 w-10 text-white ml-1">-
                         </button>
                 </div>
@@ -69,13 +92,28 @@
                 </table>
             </div>
         </div>
-    <p v-if="falsche_dim">ERROR</p>
+        
+    <!--MATRIX C-->
+    <div class="flex justify-center w-full mt-12">
+        <table v-if="show_C">
+            <tr v-for="(zeile,i) in C_matrix" :key="i">
+                <td v-for="(value, j) in zeile" :key="j">
+                <button class="bg-gray-300 w-12 text-center ml-2 mr-2 mb-1 border-black border-1">
+                    {{ C_matrix[i][j] }}
+                </button>
+                </td>
+            </tr>
+        </table>
+    </div>
    
+
     </template>
 
 <script setup lang="ts">
-    import { ref, watch } from 'vue'
-    const falsche_dim = ref(false)
+    import { ref } from 'vue'
+    const dim_multi = ref(true)
+    const dim_addition = ref(true)
+    const show_C = ref(false)
     const A_anzahl_spalten = ref(3)
     const A_anzahl_zeilen = ref(3)
     const A_matrix = ref<number[][]>(
@@ -146,7 +184,9 @@
     }
 
     function matrix_multiplikator(){
+        update_C_matrix()
         if(B_anzahl_zeilen.value === A_anzahl_spalten.value){
+            show_C.value = true
             for(let i = 0; i < A_anzahl_zeilen.value; i++){
                 for(let j = 0; j < B_anzahl_spalten.value; j++){
                     let sum = 0;
@@ -162,17 +202,50 @@
                 
     }
     function update_C_matrix() {
-        C_matrix.value = Array.from({ length: B_anzahl_zeilen.value }, () =>
-            Array.from({ length: A_anzahl_spalten.value }, () => 0)
+        C_matrix.value = Array.from({ length: A_anzahl_zeilen.value }, () =>
+            Array.from({ length: B_anzahl_spalten.value }, () => 0)
         )
+        }
+
+
+    function matrix_addierer(){
+        update_C_matrix()
+        if(A_anzahl_spalten.value === B_anzahl_spalten.value){
+            if(A_anzahl_zeilen.value === B_anzahl_zeilen.value){
+                show_C.value = true
+                for(let i = 0; i < A_anzahl_zeilen.value; i++){
+                    for(let j = 0; j < A_anzahl_spalten.value; j++){
+                        C_matrix.value[i][j] = A_matrix.value[i][j]+ B_matrix.value[i][j]; 
+                    }
+                }
+            }
+        }
+    }
+
+    function matrix_subtrahierer(){
+        update_C_matrix()
+        if(A_anzahl_spalten.value === B_anzahl_spalten.value){
+            if(A_anzahl_zeilen.value === B_anzahl_zeilen.value){
+                show_C.value = true
+                for(let i = 0; i < A_anzahl_zeilen.value; i++){
+                    for(let j = 0; j < A_anzahl_spalten.value; j++){
+                        C_matrix.value[i][j] = A_matrix.value[i][j] - B_matrix.value[i][j]; 
+                    }
+                }
+            }
+        }
+    }
+
+    function dim_checker(){
+        show_C.value = false
+        if(A_anzahl_spalten.value === B_anzahl_spalten.value){
+            if(A_anzahl_zeilen.value === B_anzahl_zeilen.value){
+                dim_addition.value = true
+        } else{dim_addition.value = false}
+    } else{dim_addition.value = false}
+
         if(A_anzahl_spalten.value === B_anzahl_zeilen.value){
-            falsche_dim.value = false
-            matrix_multiplikator();
-        } else { falsche_dim.value = true;
-
-        }
-        }
-
-    // Watcher für die Dimensionen
-    watch([A_anzahl_spalten, B_anzahl_zeilen], update_C_matrix, { immediate: true })
+            dim_multi.value = true
+        } else dim_multi.value = false
+    }    
     </script>
